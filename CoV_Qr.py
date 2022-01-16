716ee2843d345d4027d2f9a40caa3c78c4858ce9
Nekoler <2697446335@qq.com>
2022-01-16 10:26:53 +0800

    CoV_Qr

---
+++ CoV_Qr.py
@@ -0,0 +1,15 @@
+from json import dumps
+from qrcode import QRCode
+from base64 import b64encode
+
+idcard = open('id.txt')
+for i in idcard.readlines():
+    i = i.replace('\n','').split(' ')
+    out = dumps({'name':i[0],'idcard':i[1]},ensure_ascii=False,separators=(',',':'))
+    out = b64encode(out.encode()).decode()
+    Qr = QRCode()
+    Qr.border = 1
+    Qr.add_data(out)
+    Qr.make()
+    print(i[0])
+    Qr.print_ascii(tty=True)
