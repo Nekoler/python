441f360ba724eb3bc6b223e98ecc154245c5eedc
Nekoler <2697446335@qq.com>
2022-12-07 18:24:22 +0800

    更新

--- YGOcards.py
+++ YGOcards.py
@@ -1,14 +1,16 @@
 import numpy
 from PIL import Image
 
-p0 = numpy.array(Image.open(''))
-p1 = numpy.array(Image.open(''))
-p2 = numpy.array(Image.open(''))
-p3 = numpy.array(Image.open(''))
-p4 = numpy.array(Image.open(''))
-p5 = numpy.array(Image.open(''))
-p6 = numpy.array(Image.open(''))
-p7 = numpy.array(Image.open(''))
+p = ''''''.split('\n')
+
+p0 = numpy.array(Image.open('Cards/'+p[0]+'.png'))
+p1 = numpy.array(Image.open('Cards/'+p[1]+'.png'))
+p2 = numpy.array(Image.open('Cards/'+p[2]+'.png'))
+p3 = numpy.array(Image.open('Cards/'+p[3]+'.png'))
+p4 = numpy.array(Image.open('Cards/'+p[4]+'.png'))
+p5 = numpy.array(Image.open('Cards/'+p[5]+'.png'))
+p6 = numpy.array(Image.open('Cards/'+p[6]+'.png'))
+p7 = numpy.array(Image.open('Cards/'+p[7]+'.png'))
 p8 = numpy.array([0]*4*2589).reshape((2589,1,4))
 p9 = numpy.array([0]*4*7111).reshape((1,7111,4))
 
@@ -17,4 +19,4 @@ a2 = numpy.hstack((p4,p8,p5,p8,p6,p8,p7))
 a3 = numpy.vstack((a1,p9,a2))
 
 img = Image.fromarray(numpy.uint8(a3))
-img.save('')
+img.save('.png')
