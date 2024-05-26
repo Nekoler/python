eb7c4e1bf1e17f3768d73b14751b7c4906ee96a7
Nekoler <2697446335@qq.com>
2024-05-26 12:41:14 +0800

    update

--- m3u8downloader.py
+++ m3u8downloader.py
@@ -1,14 +1,17 @@
 import os
 import m3u8
+import subprocess
 from sys import argv
-from requests import get
+from Crypto.Cipher import AES
 from threading import Thread,active_count
-from Crypto.Cipher import AES,_mode_cbc
+from requests import session,Session,adapters
 
-def downloader(filepath:str,url:str):
-    with open(filepath,'wb') as ts_local,get(url) as ts_online:
-        ts_local.write(ts_online.content)
+def m3u8_handler(url:str,sess:Session):
+    with sess.get(url) as m3u8_file:
+        m3u8_text = m3u8.loads(m3u8_file.text,url)
+    return [i.absolute_uri for i in m3u8_text.segments],m3u8_text.keys
 
+<<<<<<< HEAD
 def m3u8_handler(url:str):
     m3u8_file = m3u8.loads(get(url).text,url)
     return [i.absolute_uri for i in m3u8_file.segments],m3u8_file.keys
@@ -19,12 +22,39 @@ def create_aes(key_iv:m3u8.Key):
         iv = b'0000000000000000'
     else:
         iv = key_iv.iv[2:].encode()
+=======
+def create_aes(key_iv:m3u8.Key,sess:Session):
+    with sess.get(key_iv.absolute_uri) as key_file:
+        key = key_file.content
+        if key_iv.iv:
+            iv = key_iv.iv[2:].encode()
+        else:
+            iv = b'0000000000000000'
+>>>>>>> 7bad5e7 (update)
     return AES.new(key,AES.MODE_CBC,iv)
 
-def create_dir(tmp_dir):
+def downloader(url:str,tmp_dir:str,ts_name:str,sess:Session):
+    with open(f'{tmp_dir}/{ts_name}','wb') as ts_local:
+        with sess.get(url) as ts_online:
+            ts_local.write(ts_online.content)
+
+def ts_handler(filename:str,tmp_dir:str,url_list:list,keys:m3u8.Key,sess:Session):
+    with open(f'{filename}','wb') as video:
+        if keys:
+            aes = create_aes(keys[0],sess)
+            for i in url_list:
+                with open(f"{tmp_dir}/{i.split('/')[-1]}",'rb') as ts_local:
+                    video.write(aes.decrypt(ts_local.read()))
+        else:
+            for i in url_list:
+                with open(f"{tmp_dir}/{i.split('/')[-1]}",'rb') as ts_local:
+                    video.write(ts_local.read())
+
+def create_dir(tmp_dir:str):
     if not os.path.isdir(tmp_dir):
         os.mkdir(tmp_dir)
 
+<<<<<<< HEAD
 def ts_handle(filepath:str,ts_list:list,aescodec:_mode_cbc.CbcMode|None):
     with open(filepath,'wb') as video:
         if aescodec:
@@ -44,12 +74,38 @@ create_dir(tmp_dir)
 ts_list = [tmp_dir+'/'+i.split('/')[-1] for i in segments]
 for i1,i2 in zip(ts_list,segments):
     while active_count() > 16:
-        pass
-    Thread(target=downloader,args=(i1,i2)).start()
+=======
+def remove_dir(filename:str,tmp_dir:str):
+    os.remove(f'tmp_{filename}')
+    for i in os.listdir(tmp_dir):
+        os.remove(f'{tmp_dir}/{i}')
+    os.removedirs(tmp_dir)
 
+def main(url:str,filename:str):
+    tmp_dir = f'{filename}_tmp'
+    create_dir(tmp_dir)
+    with session() as sess:
+        sess.timeout = 5
+        sess.mount('http', adapters.HTTPAdapter(max_retries=5))
+        ts_list,keys = m3u8_handler(url,sess)
+        for i in ts_list:
+            Thread(target=downloader,args=(i,tmp_dir,i.split('/')[-1],sess)).start()
+            while active_count() > 16:
+                pass
+    while active_count() != 1:
+>>>>>>> 7bad5e7 (update)
+        pass
+    ts_handler(f'tmp_{filename}',tmp_dir,ts_list,keys,sess)
+    subprocess.run(f'ffmpeg -i tmp_{filename} -c copy {filename}',stderr=subprocess.DEVNULL)
+    remove_dir(filename,tmp_dir)
+
+<<<<<<< HEAD
 while active_count() != 1:
     pass
 if key_iv:
     ts_handle(argv[2],ts_list,create_aes(key_iv[0]))
 else:
     ts_handle(argv[2],ts_list,None)
+=======
+main(argv[1],argv[2])
+>>>>>>> 7bad5e7 (update)
