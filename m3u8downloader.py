ed900bce09e29d58380950e2114647fc0b5de478
Nekoler <2697446335@qq.com>
2024-05-26 16:59:26 +0800

    update

--- m3u8downloader.py
+++ m3u8downloader.py
@@ -1,6 +1,5 @@
 import os
 import m3u8
-import subprocess
 from sys import argv
 from Crypto.Cipher import AES
 from threading import Thread,active_count
@@ -9,7 +8,7 @@ from requests import session,Session,adapters
 def m3u8_handler(url:str,sess:Session):
     with sess.get(url) as m3u8_file:
         m3u8_text = m3u8.loads(m3u8_file.text,url)
-    return [i.absolute_uri for i in m3u8_text.segments],m3u8_text.keys
+    return [i.absolute_uri for i in m3u8_text.segments],m3u8_text.keys[0]
 
 def create_aes(key_iv:m3u8.Key,sess:Session):
     with sess.get(key_iv.absolute_uri) as key_file:
@@ -25,10 +24,10 @@ def downloader(url:str,tmp_dir:str,ts_name:str,sess:Session):
         with sess.get(url) as ts_online:
             ts_local.write(ts_online.content)
 
-def ts_handler(filename:str,tmp_dir:str,url_list:list,keys:m3u8.Key,sess:Session):
+def ts_handler(filename:str,tmp_dir:str,url_list:list,key:m3u8.Key,sess:Session):
     with open(f'{filename}','wb') as video:
-        if keys:
-            aes = create_aes(keys[0],sess)
+        if key:
+            aes = create_aes(key,sess)
             for i in url_list:
                 with open(f"{tmp_dir}/{i.split('/')[-1]}",'rb') as ts_local:
                     video.write(aes.decrypt(ts_local.read()))
@@ -53,15 +52,15 @@ def main(url:str,filename:str):
     with session() as sess:
         sess.timeout = 5
         sess.mount('http', adapters.HTTPAdapter(max_retries=5))
-        ts_list,keys = m3u8_handler(url,sess)
+        ts_list,key = m3u8_handler(url,sess)
         for i in ts_list:
             Thread(target=downloader,args=(i,tmp_dir,i.split('/')[-1],sess)).start()
             while active_count() > 16:
                 pass
     while active_count() != 1:
         pass
-    ts_handler(f'tmp_{filename}',tmp_dir,ts_list,keys,sess)
-    subprocess.run(f'ffmpeg -i tmp_{filename} -c copy {filename}',stderr=subprocess.DEVNULL)
+    ts_handler(f'tmp_{filename}',tmp_dir,ts_list,key,sess)
+    os.system(f'ffmpeg -i tmp_{filename} -c copy {filename}')
     remove_dir(filename,tmp_dir)
 
 main(argv[1],argv[2])
