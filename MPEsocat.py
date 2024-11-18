f0e72c7d3f0b65b119910065d16cd2156ed99d49
Nekoler <2697446335@qq.com>
2024-11-18 11:06:20 +0800

    fix

--- MPEsocat.py
+++ MPEsocat.py
@@ -8,7 +8,4 @@ ip = psutil.net_if_addrs()['wlan2'][0][1]
 qr = qrcode.QRCode(border=1)
 qr.add_data(f'MPEsocat socat TCP-LISTEN:15567,fork TCP:{ip}:8080')
 qr.print_ascii(tty=True)
-qr = qrcode.QRCode(border=1)
-qr.add_data('http://localhost:15567/webview/markdown-preview-enhanced')
-qr.print_ascii(tty=True)
 subprocess.run(['socat','TCP-LISTEN:8080,fork','TCP:localhost:15567'])
