b45f9c472d9c3d2e5082ba1c4390dc60291251f6
Nekoler <2697446335@qq.com>
2025-05-26 22:17:45 +0800

    update

--- chconv.py
+++ chconv.py
@@ -1,16 +1,18 @@
 import sys
 import zhconv
 
-if len(sys.argv) != 3:
-    print('chconv input ouput')
+if len(sys.argv) != 2:
+    print('chconv input')
     exit()
 
 update = {'妳':'你'}
-with open(sys.argv[1],'rb') as old,open(sys.argv[2],'wb') as new:
-    temp = old.read()
+with open(sys.argv[1],'rb+') as text:
+    temp = text.read()
     try:
         temp = temp.decode('utf-8')
     except:
         temp = temp.decode('gbk')
     temp = zhconv.convert(temp,'zh-cn',update)
-    new.write(temp.encode())
+    text.seek(0)
+    text.truncate(0)
+    text.write(temp.encode())
