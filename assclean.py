eabf025a54d36231c09c327bc5da29abd7c7d36f
Nekoler <2697446335@qq.com>
2026-09-25 15:09:50 +0800

    NO [Aegisub Project Garbage]

--- assclean.py
+++ assclean.py
@@ -6,6 +6,7 @@ keys: list[str] = ["ScriptType", "PlayResX", "PlayResY"]
 
 for ass in asses:
     sub: pysubs2.SSAFile = pysubs2.load(ass)
+    sub.aegisub_project = {}
     sub.info = {i: j for i, j in sub.info.items() if i in keys}
     events: list[pysubs2.SSAEvent] = []
     for line in sub.events:
